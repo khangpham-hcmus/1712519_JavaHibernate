c9cd413017704b9662e0ac7b2210d52eb888e7bb
khangpham-hcmus <hoangkhang.hcmus.edu.110299@gmail.com>
2021-06-01 01:41:19 +0700

    01/06/20221: cm2: complete function update password for account

--- 1712519_Hibernate/src/MainPackage/Main.java
+++ 1712519_Hibernate/src/MainPackage/Main.java
@@ -12,8 +12,15 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         try {
+          /* Accounts ac= AccountsDAO.GET_ACCOUNT_THROUGH_USERNAME_TYPE("1712519",2);
+           System.out.println(ac.toString());
+           System.out.println(ac.get_student_());
+           System.out.println(ac.get_teachermanager_());*/
 
-            AccountsDAO.GET_INFORMATION_OF_ACCOUNT____("1712519",2);
+          // AccountsDAO.UPDATE_PASSWORD("1712519","khangpham");
+
+
+
         }
         catch (Exception e){
             System.out.println("Exception in main: "+e);
--- 1712519_Hibernate/src/dao/AccountsDAO.java
+++ 1712519_Hibernate/src/dao/AccountsDAO.java
@@ -1,5 +1,6 @@
 package dao;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import pojo.Accounts;
 import pojo.Students;
@@ -12,81 +13,19 @@ import java.util.List;
 //------------------------------------------------------------
 public class AccountsDAO {
     //-------------------------------------------------------
-    private static List<Accounts> GET_LIST_ACCOUNTS_IN_SYSTEM(){
-        //
-        Session ss=HibernateUtil.getSessionFactory().openSession();
-        final String HQL="from Accounts";
-        List<Accounts> _list_=null;
-        try{
-            Query q=ss.createQuery(HQL);
-            _list_=q.list();
-        }
-        catch (Exception e){
-            System.out.println("Exception in AccountsDAO: "+e);
-        }
-        finally {
-            if(ss!=null){
-                ss.close();
-            }
-        }
-        return _list_;
-    }
-    //cau1: getInformation of account:
-    public static List<String> GET_INFORMATION_OF_ACCOUNT(String _username_){
-        List<String > info=null;
-        List<Accounts> _list_=null;
-        Session ss=HibernateUtil.getSessionFactory().openSession();
-        try{
-            final String HQL="select ac from Accounts as ac where ac.userName=:name";
-            System.out.println(HQL);
-            Query q=ss.createQuery(HQL);
-            q.setParameter("name",_username_);
-            //_list_=AccountsDAO.GET_LIST_ACCOUNTS_IN_SYSTEM();
-            _list_=q.list();
-            if(_list_.size()!=0)
-            {
-                info=new ArrayList<>();
-                info.add(_list_.get(0).getUserName());
-                info.add(_list_.get(0).getPass());
-                Integer type=_list_.get(0).getTypeOfAccount();
-                info.add(type.toString());
-            }
-        }
-        catch (Exception e){
-            System.out.println("Exception in AccountsDAO: "+e);
-        }
-        finally {
-            if(ss!=null){
-                ss.close();
-            }
-        }
-        return info;
-    }
 
-    //GET TEACHERMANAGER:
-    public static Accounts GET_INFORMATION_OF_ACCOUNT____(String username, Integer _type_){
-        Session ss=HibernateUtil.getSessionFactory().openSession();
+    //GET_ACCOUNT_THROUGH_USERNAME_TYPE
+    public static Accounts GET_ACCOUNT_THROUGH_USERNAME_TYPE(String _username_,Integer _TypeOfAccount_){
         Accounts ac=null;
+        Session ss=HibernateUtil.getSessionFactory().openSession();
         try{
-            String hql="from Accounts ac where userName=:name and ac.typeOfAccount=: type";
+            String hql="from Accounts as ac where ac.userName=:name and ac.typeOfAccount=:type";
             Query q=ss.createQuery(hql);
-            q.setParameter("name",username);
-            q.setParameter("type" ,_type_) ;
-            List<Accounts> l=q.list();
-            System.out.println(l.get(0).toString());
-            if(_type_==2){
-                Students s=l.get(0).get_student_();
-                if(s==null){
-                    System.out.println("nullpointer");
-                }
-                else{
-                System.out.println(s.toString());
-                }
-            }
-
-        }
-        catch (Exception e){
-
+            q.setParameter("name",_username_);
+            q.setParameter("type",_TypeOfAccount_);
+            ac= (Accounts) q.list().get(0);
+        }catch (Exception exc){
+            System.out.println("Exception in AccountsDAO: "+exc.getMessage());
         }
         finally {
             if(ss!=null){
@@ -95,36 +34,40 @@ public class AccountsDAO {
         }
         return ac;
     }
-
-    //-----------------------------------------------------------------------------------------
-
-    //cau3: change the password:
-
-    //get all Accounts
-    private static List<Accounts> getAllAccounts(){
-        //open session
-        Session ss= HibernateUtil.getSessionFactory().openSession();
-        List<Accounts> listAccounts=null;
+    //UPDATE_PASSWORD:
+    public static boolean UPDATE_PASSWORD(String _username_,String newpass){
+        Session ss=HibernateUtil.getSessionFactory().openSession();
+        boolean checkupdate=false;
+        Transaction tx=null;
         try{
-            //Query statement:
-            final String hbl="select ac from Accounts as ac";
-            Query q=ss.createQuery(hbl);
-            //Get result set:
-            listAccounts=q.list();
+            tx=ss.beginTransaction();
+            String hql="update Accounts set Pass=:passnew where UserName=:username ";
+            Query q=ss.createSQLQuery(hql);
+            q.setParameter("passnew",newpass);
+            q.setParameter("username",_username_);
+            Integer i=q.executeUpdate();
+            tx.commit();
+            if(i!=0)
+                checkupdate=true;
+            else
+                checkupdate=false;
+            System.out.println(checkupdate);
         }
         catch (Exception e){
-            System.out.println("Exception in AccountDAO: "+e.getMessage());
+            System.out.println("Exception in AccountsDAO: "+e.getMessage());
         }
         finally {
-            ss.close();
+            if(ss!=null)
+                ss.close();
         }
-        return listAccounts;
+        return checkupdate;
     }
+
     //check Login
     public static boolean Login(String _username_,String _password_,Integer _type_){
         boolean flag=false;
         Session ss=HibernateUtil.getSessionFactory().openSession();
-        List<Accounts> listAccountsLogin=getAllAccounts();
+        List<Accounts> listAccountsLogin=null;//getAllAccounts();
         try{
             if(listAccountsLogin.size()!=0) {
                 for(Accounts e:listAccountsLogin){
